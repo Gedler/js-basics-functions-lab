a9b6623689f3b50c5a61edf3a328ac0e75bc0f4a
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-14 14:17:07 -0400

    commit tree 22645d10312b3cab76d9f872dac8a746ebdcc509

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(x,y) {
+    return x-y ;
+}
+
+distanceFromHqInBlocks(43,42);
+distanceFromHqInBlocks(50,42);
+distanceFromHqInBlocks(42,34);
